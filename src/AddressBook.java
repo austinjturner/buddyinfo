6940e6ebf5966515b6813a7fcc5daa9ae780cbe9
austinturner <austinturner@cb5109-30.labs.sce.carleton.ca>
2017-10-02 08:53:31 -0400

    First Commit

---
+++ src/AddressBook.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+	private List<BuddyInfo> buddyList;
+	
+	
+	public void addBuddy(BuddyInfo buddyToAdd) {
+		buddyList.add(buddyToAdd);
+	}
+	
+	public BuddyInfo removeBuddy(int index) {
+		if (index >= 0 && index < this.buddyList.size()) {
+			return buddyList.remove(index);
+		}
+		return null;
+	}
+	
+	public AddressBook() {
+		this.buddyList = new ArrayList<BuddyInfo>();
+	}
+	
+	public static void main(String[] args) {
+		BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "1234");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addBuddy(buddy);
+		addressBook.removeBuddy(0);
+	}
+}
